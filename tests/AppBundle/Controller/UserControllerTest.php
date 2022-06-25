09849b5fd051d6b35087ec548b74e8d4f37c60fc
Belarif <b.ocine@live.fr>
2022-06-25 15:18:59 +0200

    TDL4: functional test of user create page display

---
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class UserControllerTest extends WebTestCase
+{
+    public function test_it_should_display_user_create_page()
+    {
+        $client = self::createClient();
+
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $urlGenerator->generate('user_create'));
+
+        $response = $client->getResponse();
+        self::assertTrue($response->isOk());
+        self::assertGreaterThan(
+            0,
+            $crawler->filter('form')->count());
+        self::assertGreaterThan(
+            0,
+            $crawler->filter('input[id=user_username]')->count()
+        );
+        self::assertGreaterThan(
+            0,
+            $crawler->filter('input[id=user_password_first]')->count()
+        );
+        self::assertGreaterThan(
+            0,
+            $crawler->filter('input[id=user_password_second]')->count()
+        );
+        self::assertGreaterThan(
+            0,
+            $crawler->filter('input[id=user_email]')->count()
+        );
+        self::assertNotNull($crawler->selectButton('submit'));
+    }
+}
